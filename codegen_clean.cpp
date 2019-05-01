cc02d75c5f88c96f458f5b835bf86018873e3c4e
MikeCAT <mikecat@users.noreply.github.com>
2019-05-01 23:44:24 +0900

    add merge_generated_labels()

--- codegen_clean.cpp
+++ codegen_clean.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <map>
 #include "codegen.hpp"
 #include "codegen_internal.hpp"
 
@@ -67,6 +68,42 @@ static bool remove_unused_generated_labels(std::vector<asm_inst>& insts) {
 	return progress_exists;
 }
 
+// 連続した自動生成ラベルを1個にまとめる
+static bool merge_generated_labels(std::vector<asm_inst>& insts) {
+	bool progress_exists = false;
+	std::map<std::string, std::string> rewrite_map;
+	// 書き換え関係を調査する
+	bool rewriting = false;
+	std::string rewrite_to = "";
+	for (auto itr = insts.begin(); itr != insts.end(); itr++) {
+		if (itr->kind == LABEL && itr->label[0] == '_' && itr->label[1] == '_') {
+			// 自動生成ラベル
+			if (rewriting) {
+				// 2番目以降なら、最初のラベルに書き換える指示を出す
+				rewrite_map[itr->label] = rewrite_to;
+			} else {
+				// 最初なら、書き換え先として登録する
+				rewriting = true;
+				rewrite_to = itr->label;
+			}
+		} else if (itr->kind != EMPTY) {
+			// 自動生成ラベル以外のものが来たら、書き換えを止める
+			rewriting = false;
+		}
+	}
+	// 書き換えを実行する
+	for (auto itr = insts.begin(); itr != insts.end(); itr++) {
+		if (itr->label != "" &&
+		(itr->kind == JCC || itr->kind == JMP_DIRECT || itr->kind == CALL_DIRECT)) {
+			if (rewrite_map.find(itr->label) != rewrite_map.end()) {
+				itr->label = rewrite_map[itr->label];
+				progress_exists = true;
+			}
+		}
+	}
+	return progress_exists;
+}
+
 // 生成したコードを改善する
 void codegen_clean(std::vector<asm_inst>& insts) {
 	bool progress_exists;
@@ -74,5 +111,6 @@ void codegen_clean(std::vector<asm_inst>& insts) {
 		progress_exists = false;
 		if (remove_jump_to_next(insts)) progress_exists = true;
 		if (remove_unused_generated_labels(insts)) progress_exists = true;
+		if (merge_generated_labels(insts)) progress_exists = true;
 	} while (progress_exists);
 }
